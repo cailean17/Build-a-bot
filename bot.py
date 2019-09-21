377f879f8142afd5433a8a34f4c05f1a849dc585
cailean17 <55571023+cailean17@users.noreply.github.com>
2019-09-20 20:43:44 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,9 @@ from esipysi import EsiAuth
 import asyncio
 import aiohttp
 import time
+from asgiref.sync import sync_to_async
 
+esi = EsiPysi("https://esi.evetech.net/_latest/swagger.json?datasource=tranquility", user_agent="Cailean Blackies")
 
 
 
@@ -27,11 +29,14 @@ headers = {
     'Authorization': 'Basic MGM4YzI0MTYwY2QyNDFiMmI2N2EzYzRlM2I2NDkxNjU6RUZLRHZNVVpSeHZvYU14UHNIeXJwNEduZzU3Z0lOM2xrZ1dnYUlZYQ==',
 }
 
-data = '{"grant_type":"authorization_code", "code":"_0qJOz47pKnEX9eWenXXamiaDQlhM3fxz_fYs_XCzd1Y9xbhSEWjgNl3Qro7xAn-"}'
+data = '{"grant_type":"authorization_code", "code":"0Na3WjTPCWe6DMFFKZwYKlBGSTf7EzIlwYSqfLZuF_Vu6rKkXzRs9sgnI21OSicU"}'
 
 response = requests.post('https://login.eveonline.com/oauth/token', headers=headers, data=data)
 
 print(response.text)
+
+
+
 
 
 
@@ -57,19 +62,19 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if ( message.content == eventstring1):
+    if( message.content == eventstring1):
     
-        #this checks if the the eventstring!(!structures) has been sent in discord chat then sends a message and calls the api to gain information
-        #right now im trying to get simple character info from eve.
-        #Error shows up saying   "op =  await esi.get_operation("get_search") TypeError: object EsiOp can't be used in 'await' expression"
+          #dont have required corp roles in game to view full list of structures and other info.
+         op = esi.get_operation("get_corporations_corporation_id_structures")
+         auth = await EsiAuth.from_refresh_token("0c8c24160cd241b2b67a3c4e3b649165", "EFKDvMUZRxvoaMxPsHyrp4Gng57gIN3lkgWgaIYa", "1aiS0Kg3d8Xqk6T3ky1aAID81R7fE6D_ZWgEHJbQ55c")
+         op.set_auth(auth)
+         result =  await op.execute(corporation_id = "98565967")
+
+       
+
+
+
         
-        await message.channel.send("Pulling Structure Info")
-        esi = EsiPysi("https://esi.evetech.net/_latest/swagger.json?datasource=tranquility", user_agent="Cailean Blackies")
-        op =  await esi.get_operation("get_search")
-        auth =  await  EsiAuth.from_refresh_token("0c8c24160cd241b2b67a3c4e3b649165", "EFKDvMUZRxvoaMxPsHyrp4Gng57gIN3lkgWgaIYa", "fPV3YRBc8hvU6Cs22_tENN8cm3VEoHrmpxYTMLDZT_A")
-        op.set_auth(auth)
-        result = await op.execute(categories="character", search="Cailean BLACKIES")
-        print(result)
 
         
                                    
